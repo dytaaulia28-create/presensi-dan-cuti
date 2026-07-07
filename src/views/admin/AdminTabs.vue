cf3546f373274f378dcc0186f3bf4eebc5dda4b7
dytaaulia28-create <dytaaulia28@gmail.com>
2026-07-07 15:38:11 +0700

    Update AdminTabs.vue

--- src/views/admin/AdminTabs.vue
+++ src/views/admin/AdminTabs.vue
@@ -3,25 +3,17 @@
 		<ion-tabs>
 			<ion-router-outlet />
 			<ion-tab-bar slot="bottom">
-				<ion-tab-button tab="beranda" href="/admin/beranda">
-					<ion-icon :icon="gridOutline" />
-					<ion-label>Beranda</ion-label>
-				</ion-tab-button>
-				<ion-tab-button tab="karyawan" href="/admin/karyawan">
+				<ion-tab-button tab="akun" href="/admin/akun">
 					<ion-icon :icon="peopleOutline" />
-					<ion-label>Karyawan</ion-label>
+					<ion-label>Akun</ion-label>
 				</ion-tab-button>
-				<ion-tab-button tab="absensi" href="/admin/absensi">
+				<ion-tab-button tab="log" href="/admin/log">
 					<ion-icon :icon="listOutline" />
-					<ion-label>Absensi</ion-label>
+					<ion-label>Log Aktivitas</ion-label>
 				</ion-tab-button>
-				<ion-tab-button tab="cuti" href="/admin/cuti">
-					<ion-icon :icon="checkmarkDoneOutline" />
-					<ion-label>Cuti</ion-label>
-				</ion-tab-button>
-				<ion-tab-button tab="laporan" href="/admin/laporan">
-					<ion-icon :icon="barChartOutline" />
-					<ion-label>Laporan</ion-label>
+				<ion-tab-button tab="profil" href="/admin/profil">
+					<ion-icon :icon="personOutline" />
+					<ion-label>Profil</ion-label>
 				</ion-tab-button>
 			</ion-tab-bar>
 		</ion-tabs>
@@ -33,6 +25,6 @@ import {
 	IonPage, IonTabs, IonRouterOutlet, IonTabBar, IonTabButton, IonIcon, IonLabel,
 } from "@ionic/vue"
 import {
-	gridOutline, peopleOutline, listOutline, checkmarkDoneOutline, barChartOutline,
+	peopleOutline, listOutline, personOutline,
 } from "ionicons/icons"
 </script>
